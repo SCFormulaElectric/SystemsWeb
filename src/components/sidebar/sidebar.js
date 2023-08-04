f879bc2ee5cb839a7c1aae7a10335ef220567726
Nicky Hama <njhama@usc.edu>
2023-08-04 02:53:57 -0700

    added more componenets + sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { Typography } from './Typography.tsx';
 
 //icons
 import HomeIcon from '../../icons/home.js'
+import logo from '../../icons/logo.webp'
+
+import './sidebar.css'
+
 //import icons from icons folder 
 //https://github.com/azouaoui-med/react-pro-sidebar/blob/master/storybook/Playground.tsx
 //cant import the css outside of src
@@ -12,32 +17,85 @@ import HomeIcon from '../../icons/home.js'
 
 function Sidebar1() {
   return (
-    <Sidebar>
+    <div>
+      <Sidebar>
+      <img src={logo} className='icon-fit'/>
+      <Typography
+        variant="body2"
+        fontWeight={600}
+        className='small-title'
+      >
+        General
+      </Typography>
       <Menu iconShape="square">
-        <MenuItem
-        icon = {<HomeIcon/>}
-        >Home</MenuItem>
-            <SubMenu 
-                title="Components"
-                icon = {<HomeIcon/>}
-                label = "Onboarding"
-                
-            >
-                <MenuItem>To Do</MenuItem>
-                <MenuItem>Github</MenuItem>
-                <MenuItem>Google Drive</MenuItem>
-                <MenuItem>Github</MenuItem>
-            </SubMenu>
-            <SubMenu
-                title = "Team"
-                label = "Team"
-            >
-                <MenuItem>Tytionex</MenuItem>
-                <MenuItem>Contact</MenuItem>
 
-            </SubMenu>
+        {/*HOME*/}
+        <SubMenu
+          label = "Home"
+          icon = {<HomeIcon/>}
+          className='sidebar-item'
+        >
+        </SubMenu>
+
+        {/*ONBOARDING*/}
+        <SubMenu 
+          className='sidebar-item'
+          title="Components"
+          icon = {<HomeIcon/>}
+          label = "Onboarding"
+        >
+          <MenuItem>To Do</MenuItem>
+          <MenuItem>Github</MenuItem>
+          <MenuItem>Google Drive</MenuItem>
+          <MenuItem>Github</MenuItem>
+        </SubMenu>
+
+        {/*TEAM*/}
+        <SubMenu
+          title = "Team"
+          label = "Team"
+          className='sidebar-item'
+          icon = {<HomeIcon/>}
+        >
+          <MenuItem>Tytionex</MenuItem>
+          <MenuItem>Contact</MenuItem>
+        </SubMenu>
+
+        {/*RESOURCE SUBTITLE*/}
+        <div style={{ padding: '0 24px', marginBottom: '8px', marginTop: '32px' }}></div>
+        <Typography
+          variant="body2"
+          fontWeight={600}
+          className='small-title'
+        >
+          Resources
+        </Typography>
+
+        {/*CALENDAR*/}
+        <MenuItem
+          icon = {<HomeIcon/>}
+        >
+          Calendar
+        </MenuItem>
+
+        {/*MISC SUBTITLE*/}
+        <div style={{ padding: '0 24px', marginBottom: '8px', marginTop: '32px' }}></div>
+        <Typography
+          variant="body2"
+          fontWeight={600}
+          className='small-title'
+        >
+          Misc
+        </Typography>
+        <MenuItem
+          icon = {<HomeIcon/>}
+        >
+          Random
+        </MenuItem>
       </Menu>
     </Sidebar>
+    </div>
+    
   );
 }
 
