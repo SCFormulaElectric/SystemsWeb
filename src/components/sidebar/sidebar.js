87266f346a054a546004bcc034bae87b865b6348
Nicky Hama <njhama@usc.edu>
2023-08-03 22:56:49 -0700

    did the sidebar

---
+++ src/components/sidebar/sidebar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+//import icons from icons folder 
+//https://github.com/azouaoui-med/react-pro-sidebar/blob/master/storybook/Playground.tsx
+//cant import the css outside of src
+//might have to manually copy the css to src
+//import '../../node_modules/react-pro-sidebar/dist/css/styles.css';
+//import 'react-pro-sidebar/dist/css/styles.css';
+
+function Sidebar1() {
+  return (
+    <Sidebar>
+      <Menu iconShape="square">
+        <MenuItem>Home</MenuItem>
+            <SubMenu 
+                title="Components"
+                label = "Onboarding"
+            >
+                <MenuItem>To Do</MenuItem>
+                <MenuItem>Github</MenuItem>
+                <MenuItem>Google Drive</MenuItem>
+            </SubMenu>
+            <SubMenu
+                title = "Team"
+            >
+                <MenuItem>Tytionex</MenuItem>
+                <MenuItem>Contact</MenuItem>
+
+            </SubMenu>
+      </Menu>
+    </Sidebar>
+  );
+}
+
+export default Sidebar1;
---
+++ src/pages/home/home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <header>
+        <h1>hi tyler!</h1>
+      </header>
+      <main>
+        <p>This is such a great website</p>
+        <p>Bo</p>
+      </main>
+      <footer>
+        <p>&copy; {new Date().getFullYear()} nicky</p>
+      </footer>
+    </div>
+  );
+}
+
+export default HomePage;
